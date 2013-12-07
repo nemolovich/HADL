ed75ac61cc1d7365c4b76794f55330d0426d5af2
Nemolovich <nemolovich@hotmail.fr>
2013-12-07 17:49:39 +0100

    Added some specifications for component

--- src/m1/server/authentification/ConnectionManager.java
+++ src/m1/server/authentification/ConnectionManager.java
@@ -1,11 +1,13 @@
 package m1.server.authentification;
 
 import m2.element.Component;
+import m2.element.Configuration;
 
 public class ConnectionManager extends Component {
 
-	public ConnectionManager(String name, AuthProvidedPort port) {
-		super(name, port);
+	public ConnectionManager(String name, Configuration configuration,
+			AuthProvidedPort port) {
+		super(name, configuration, port);
 	}
 
 }
--- src/m2/element/Component.java
+++ src/m2/element/Component.java
@@ -3,43 +3,61 @@ package m2.element;
 import java.util.ArrayList;
 import java.util.List;
 
+import m2.M2Object;
 import m2.interfaces.Interface;
 import m2.link.Link;
 
 /**
  * 
  * @author Guillaume Coutable, Brian Gohier
- *
+ * 
  */
 public abstract class Component extends Element {
-	
-	private List<Link> links;
-	private List<Interface> interfaces;
 
-	public Component(String name, Interface intfce) {
+	protected List<Link> links;
+	protected List<Interface> interfaces;
+	private Configuration configuration;
+
+	public Component(String name, Configuration conf, Interface intfce) {
 		super(name);
-		this.interfaces=new ArrayList<Interface>();
+		this.configuration = conf;
+		this.interfaces = new ArrayList<Interface>();
 		this.addInterface(intfce);
 	}
-	
+
+	public void getMessage(M2Object message, Element from) {
+		System.out.println("[" + this.getName() + "] Receving message {"
+				+ message.getName() + "}");
+		this.configuration.sendMessage(message, this);
+	}
+
+	public void sendMessage(M2Object message) {
+		System.out.println("[" + this.getName() + "] Sending message {"
+				+ message.getName() + "}");
+	}
+
 	/*
 	 * Links
 	 */
-	
+
 	/**
 	 * Check if this configuration contains the specified link
-	 * @param link whose presence in this configuration is to be tested
+	 * 
+	 * @param link
+	 *            whose presence in this configuration is to be tested
 	 * @return {@code true} if this configuration contain the specified link
 	 */
 	public boolean containsLink(Link link) {
 		synchronized (this.links) {
-			return this.links.contains(link);			
+			return this.links.contains(link);
 		}
 	}
-	
+
 	/**
 	 * Add link
-	 * @param link to add
+	 * 
+	 * @param link
+	 *            to add
 	 * @return {@code true} if the link had been added
 	 */
 	public boolean addLink(Link link) {
@@ -47,10 +65,12 @@ public abstract class Component extends Element {
 			return this.links.add(link);
 		}
 	}
-	
+
 	/**
 	 * Add a list of links
-	 * @param links to add
+	 * 
+	 * @param links
+	 *            to add
 	 * @return {@code true} if links had all been added
 	 */
 	public boolean addLinks(List<Link> links) {
@@ -58,25 +78,29 @@ public abstract class Component extends Element {
 			return this.links.addAll(links);
 		}
 	}
-	
+
 	/**
 	 * Add links
-	 * @param links to add
+	 * 
+	 * @param links
+	 *            to add
 	 * @return {@code true} if links had all been added
 	 */
 	public boolean addLinks(Link... links) {
-		boolean added=true;
+		boolean added = true;
 		synchronized (this.links) {
-			for(Link l:links) {
-				added&=this.links.add(l);
+			for (Link l : links) {
+				added &= this.links.add(l);
 			}
 		}
 		return added;
 	}
-	
+
 	/**
 	 * remove the link from this configuration
-	 * @param link to remove
+	 * 
+	 * @param link
+	 *            to remove
 	 * @return {@code true} if this configuration contained the specified link
 	 */
 	public boolean removeLink(Link link) {
@@ -84,24 +108,27 @@ public abstract class Component extends Element {
 			return this.links.remove(link);
 		}
 	}
-	
+
 	/*
 	 * Interfaces
 	 */
-	
+
 	/**
 	 * Check if this configuration contains the specified interface
+	 * 
 	 * @param interface whose presence in this configuration is to be tested
-	 * @return {@code true} if this configuration contain the specified interface
+	 * @return {@code true} if this configuration contain the specified
+	 *         interface
 	 */
 	public boolean containsInterface(Interface intfce) {
 		synchronized (this.interfaces) {
-			return this.interfaces.contains(intfce);			
+			return this.interfaces.contains(intfce);
 		}
 	}
-	
+
 	/**
 	 * Add interface
+	 * 
 	 * @param interface to add
 	 * @return {@code true} if the interface had been added
 	 */
@@ -110,10 +137,12 @@ public abstract class Component extends Element {
 			return this.interfaces.add(intfce);
 		}
 	}
-	
+
 	/**
 	 * Add a list of interfaces
-	 * @param interfaces to add
+	 * 
+	 * @param interfaces
+	 *            to add
 	 * @return {@code true} if interfaces had all been added
 	 */
 	public boolean addInterfaces(List<Interface> interfaces) {
@@ -121,26 +150,30 @@ public abstract class Component extends Element {
 			return this.interfaces.addAll(interfaces);
 		}
 	}
-	
+
 	/**
 	 * Add interfaces
-	 * @param interfaces to add
+	 * 
+	 * @param interfaces
+	 *            to add
 	 * @return {@code true} if interfaces had all been added
 	 */
 	public boolean addInterfaces(Interface... interfaces) {
-		boolean added=true;
+		boolean added = true;
 		synchronized (this.interfaces) {
-			for(Interface l:interfaces) {
-				added&=this.interfaces.add(l);
+			for (Interface l : interfaces) {
+				added &= this.interfaces.add(l);
 			}
 		}
 		return added;
 	}
-	
+
 	/**
 	 * remove the interface from this configuration
+	 * 
 	 * @param interface to remove
-	 * @return {@code true} if this configuration contained the specified interface
+	 * @return {@code true} if this configuration contained the specified
+	 *         interface
 	 */
 	public boolean removeInterface(Interface intfce) {
 		synchronized (this.interfaces) {
