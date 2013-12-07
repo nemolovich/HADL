b2d4c168b03cd62bf536cb0cf1b71d7e492aa1dc
guiguilalane <poissondu44@hotmail.fr>
2013-12-07 18:11:24 +0100

    je sens venir les conflits...

--- src/m0/main/Main.java
+++ src/m0/main/Main.java
@@ -3,7 +3,6 @@ package m0.main;
 import m1.configuration.GlobalConfiguration;
 import m1.configuration.RPCPort;
 import m1.server.RPCServeurPort;
-import m1.server.ServeurConfiguration;
 import m2.interfaces.InterfaceType;
 
 public class Main {
@@ -17,10 +16,6 @@ public class Main {
 
 		RPCServeurPort rpcServeurPort = new RPCServeurPort("rpc-serveur-port",
 				InterfaceType.PROVIDED);
-		ServeurConfiguration serveur = new ServeurConfiguration(
-				"serveurConfiguration", rpcServeurPort);
-
-		globalConfiguration.addElement(serveur);
 
 	}
 }
